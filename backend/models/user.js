197928a288bd6240de431f1f38ba2d6e75e76e4d
JLStudioOficiall <jlstudios131@gmail.com>
2025-11-30 20:21:13 +0100

    Create user.js

---
+++ backend/models/user.js
@@ -0,0 +1,9 @@
+// backend/models/user.js
+export class User {
+  constructor(id, name, email, passwordHash) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.passwordHash = passwordHash;
+  }
+}
