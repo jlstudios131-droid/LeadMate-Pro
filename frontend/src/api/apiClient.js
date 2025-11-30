db261102f5814dd82f3848cc28ffdeb983a1504d
JLStudioOficiall <jlstudios131@gmail.com>
2025-11-30 20:54:30 +0100

    Create apiClient.js

---
+++ frontend/src/api/apiClient.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api'; // Mude para seu backend deployado
+
+const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete apiClient.defaults.headers.common['Authorization'];
+  }
+};
+
+export default apiClient;
