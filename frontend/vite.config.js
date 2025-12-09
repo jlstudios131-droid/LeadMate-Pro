fe92ab67392bf75971524129d8e48fa07a014db7
JLStudioOficiall <jlstudios131@gmail.com>
2025-12-09 07:17:45 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,6 @@ export default defineConfig({
         manualChunks: {
           react: ['react', 'react-dom'],
           router: ['react-router-dom'],
-          charts: ['recharts'],
           motion: ['framer-motion']
         }
       }
